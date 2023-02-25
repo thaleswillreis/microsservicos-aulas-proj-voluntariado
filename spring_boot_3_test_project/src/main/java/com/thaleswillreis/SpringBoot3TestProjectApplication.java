049b5b029d174f40078ded27e36cf750701f7212
thaleswillreis <thaleswill@yahoo.com.br>
2023-02-25 10:50:13 -0300

    Spring Boot 3 project created

---
+++ spring_boot_3_test_project/src/main/java/com/thaleswillreis/SpringBoot3TestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thaleswillreis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3TestProjectApplication.class, args);
+	}
+
+}
